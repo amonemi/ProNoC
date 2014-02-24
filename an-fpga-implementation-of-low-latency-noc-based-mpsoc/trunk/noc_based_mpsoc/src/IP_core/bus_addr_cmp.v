0305731d2e7f62e85afbed019b60b332686953bd
alirezamonemi <alirezamonemi@162ead16-ce4a-4b09-ba39-8ccd87514882>
2014-02-24 06:11:52 +0000

    update bus_addr_cmp.v file

--- an-fpga-implementation-of-low-latency-noc-based-mpsoc/trunk/noc_based_mpsoc/src/IP_core/bus_addr_cmp.v
+++ an-fpga-implementation-of-low-latency-noc-based-mpsoc/trunk/noc_based_mpsoc/src/IP_core/bus_addr_cmp.v
@@ -1,3 +1,32 @@
+/*********************************************************************
+							
+	File: bus_addr_cmp.v 
+	
+	Copyright (C) 2014  Alireza Monemi
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+	
+	
+	Purpose:
+	wishbone bus address comparator
+
+	Info: monemi@fkegraduate.utm.my
+
+****************************************************************/
+
+
+
 module bus_addr_cmp #(
 	parameter	RAM_EN		=	1,
 	parameter 	NOC_EN		=	1,
@@ -5,7 +34,7 @@ module bus_addr_cmp #(
 	parameter 	EXT_INT_EN	=	1,
 	parameter 	TIMER_EN		=	1,
 	parameter 	INT_CTRL_EN	=	1,
-	
+	//parameter NEW_DEV_EN  = 1,
 	parameter  	ADDR_PERFIX_	=	8,
 	parameter	SLAVE_NUM_	=	3	 
 	)
@@ -49,8 +78,13 @@ module bus_addr_cmp #(
 					assign   base_start_addr [k] = INT_CTRL_ADDR_START;
 					assign	base_end_addr 	 [k] = INT_CTRL_ADDR_START+ INT_CTRL_BK_NUM;
 				end  
-				
-				
+				/*
+				adding new device:
+				end else if(k == NEW_DEV_ID) begin 
+					assign   base_start_addr [k] = NEW_DEV__ADDR_START;
+					assign	base_end_addr 	 [k] = NEW_DEV__ADDR_START+ NEW_DEV_BK_NUM;
+				end  
+				*/
 				assign 	cmp_out[k] = (  addr_in	 >= base_start_addr [k]  ) & 	(  addr_in	   < base_end_addr[k] );
 				
 		end//for
@@ -58,3 +92,4 @@ module bus_addr_cmp #(
 
 
 endmodule
+
