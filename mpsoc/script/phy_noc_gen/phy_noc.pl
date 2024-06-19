7f300d8938ecc533a3fdf56d0b70d8ef037418d6
amonemi <alireza.monemi@bsc.es>
2024-06-20 01:14:23 +0200

    phy_noc.pl passes verilator compilation

--- mpsoc/script/phy_noc_gen/phy_noc.pl
+++ mpsoc/script/phy_noc_gen/phy_noc.pl
@@ -15,6 +15,10 @@ use strict;
 use warnings;
 use List::MoreUtils qw(uniq);
 
+use File::Basename;
+my $dirname = dirname(__FILE__);
+my $noc_dir = "$dirname/../../rtl/src_noc";
+
 
 my $noc_id = $ARGV[0];
 my $out_dir= $ARGV[1];
@@ -34,17 +38,13 @@ if ($noc_id =~ /[^a-zA-Z0-9_\$]+/){
 		 die "NOC_ID ($noc_id) contains the illegal character of \"$w[1]\" after $w[0]. Identifiers may contain alphabetic characters, numeric characters, the underscore, and the dollar sign (a-z A-Z 0-9 _ \$ )\n";
 
 }
-
+$noc_id="N$noc_id";
 
 my %replace;
-$replace{"`NOC_CONF"} = "import pronoc_pkg::*;";
+$replace{"`NOC_CONF"} = "import pronoc_pkg_${noc_id}::*;";
 $replace{"noc_localparam.v"} = "noc_localparam_${noc_id}.v";
 $replace{"topology_localparam.v"} = "topology_localparam_${noc_id}.v";
 $replace{"pronoc_pkg"} = "pronoc_pkg_${noc_id}";
-
-#variable for capturing parameters/localparams
-my $before = qr/[,=><\/\n\s\[\{\}\(\+\-\*\\\.]/;
-my $after  = qr/[,=><\/\s;\]\)\{\}\+\-\*\\\^]/;
 
 
 #create out dir
@@ -52,12 +52,12 @@ my $after  = qr/[,=><\/\s;\]\)\{\}\+\-\*\\\^]/;
 system("mkdir -p $out_dir");
 
 #get list of all files
-my $noc_dir = "/home/alireza/work/git/hca_git/git-hub/ProNoC/mpsoc/rtl/src_noc";
 opendir(my $dir, $noc_dir) or die "Could not open $noc_dir for reading: $!\n";
 my @files = readdir($dir);
 closedir($dir);
 # Filter out '.' and '..' special entries
 @files = grep { $_ ne '.' && $_ ne '..' } @files;
+
 
 #get list of all modules
 my @module_names;
@@ -78,38 +78,86 @@ foreach my $file (@files) {
 my @param_list;
 
 my @param_files=("noc_localparam.v","pronoc_pkg.sv","topology_localparam.v");
-
 for my $filename (@param_files){
-
     open my $fh, '<', "$noc_dir/$filename" or die "Cannot open file '$filename': $!\n";
     my $file_content = do { local $/; <$fh> };
     close $fh;
-
     # Remove single-line and multi-line comments
     $file_content =~ s{//.*$}{}mg;  # Remove single-line comments
     $file_content =~ s{/\*.*?\*/}{}sg;  # Remove multi-line comments
-
     # Remove content within quotes
     $file_content =~ s/"(?:[^"\\]|\\.)*"//g;
-
     # Find all parameters and localparams
     while ($file_content =~ /\b(parameter|localparam)\s+(.*?);/sg) {
         my $declaration = $2;
-
         # Split the declaration into individual parameter assignments
         my @params = split /,\s*/, $declaration;
-
         foreach my $param (@params) {
             # Extract the parameter name
             if ($param =~ /^\s*(\w+)/) {
-               
                 push @param_list ,$1;
             }
         }
     }
 }
+my @replaces = uniq @param_list;
 
-my @unique_params = uniq @param_list;
+
+#get the list of structs
+    open my $fh, '<', "$noc_dir/pronoc_pkg.sv" or die "Cannot open file pronoc_pkg.sv: $!\n";
+    my $file_content = do { local $/; <$fh> };
+    close $fh;
+    # Remove single-line and multi-line comments
+    $file_content =~ s{//.*$}{}mg;  # Remove single-line comments
+    $file_content =~ s{/\*.*?\*/}{}sg;  # Remove multi-line comments
+    # Remove content within quotes
+    $file_content =~ s/"(?:[^"\\]|\\.)*"//g;
+    # Find all structs
+    while ($file_content =~ /typedef\s+struct\s+packed\s*{.*?}\s*(\w+)\s*;/sg) {
+     my $struct_name = $1;
+     push @replaces, $struct_name;    
+    }
+
+
+#get list of functions
+    open $fh, '<', "$noc_dir/topology_localparam.v" or die "Cannot open file pronoc_pkg.sv: $!\n";
+    $file_content = do { local $/; <$fh> };
+    close $fh;
+    # Remove single-line and multi-line comments
+    $file_content =~ s{//.*$}{}mg;  # Remove single-line comments
+    $file_content =~ s{/\*.*?\*/}{}sg;  # Remove multi-line comments
+    # Remove content within quotes
+    $file_content =~ s/"(?:[^"\\]|\\.)*"//g;
+    # Find all structs
+    while ($file_content =~ /function\s+automatic\s+integer\s+(\w+)\s*;/sg) {
+       my $func = $1;
+       push @replaces, $func;      
+    }
+
+
+
+
+# Pre-compile regular expressions
+my $before = qr/[%!~,=><:\/\n\s\[\]\{\}\(\)\+\-\*\\\.]/;
+my $after  = qr/[%!~,=><:\/\s;\[\]\(\)\{\}\+\-\*\\\^]/;
+
+# Compile module replacement regex
+my %module_replacements = map { $_ => "${_}_$noc_id" } @module_names;
+my $module_regex = join '|', map { quotemeta } @module_names;
+
+# Compile file replacement regex
+my %file_replacements = map { 
+    my ($file_name, $extension) = /^(.+)\.(\w+)$/;
+    $_ => "${file_name}_${noc_id}.$extension"
+} @files;
+my $file_regex = join '|', map { quotemeta } @files;
+
+# Compile key replacement regex
+my %key_replacements = map { $_ => "${_}_${noc_id}" } @replaces;
+my $key_regex = join '|', map { quotemeta } @replaces;
+
+# Compile replace hash regex
+my $replace_regex = join '|', map { quotemeta } keys %replace;
 
 
 foreach my $file (@files) {
@@ -124,39 +172,34 @@ foreach my $file (@files) {
     if($file eq "pronoc_pkg.sv"){
          print $output_fh "`define IMPORT_PRONOC_PCK\n`define PRONOC_PKG\n";
     }
-    # Read the input file line by line
-    while (my $line = <$input_fh>) {
-       # Iterate through each module name and replace it with the modified version
-        foreach my $module (@module_names) {
-            my $replacement = "${module}_$noc_id";
-            #in each file add noc id to module names
-            $line =~ s/\b\Q$module\E\b/$replacement/g;
-            
-        }
-       foreach my $key (sort keys %replace){
-        #replace NoC_CONF with import noc_id package
-            my $replacement=$replace{$key};
-            $line =~ s/\Q$key\E/$replacement/g;  
-       }
-       foreach my $file (@files){
-            my ($file_name, $extension) = $file =~ /^(.+)\.(\w+)$/;
-            my $replacement="${file_name}_${noc_id}.$extension";
-            $line =~ s/\Q$file\E/$replacement/g;  
-       }
-       foreach my $key (@unique_params){
-            my $replacement="${key}_${noc_id}";
-            # Replace key with replacment based on the conditions
-            while ($line =~ s/($before)($key)($after)/$1$replacement$3/g) {}   
-       }
-          
-        # Write the modified line to the output file
-        print $output_fh $line;
-    }
+   
+
+# Read the input file line by line
+while (my $line = <$input_fh>) {
+    # Replace module names
+    $line =~ s/\b($module_regex)\b/$module_replacements{$1}/g;
+
+    # Replace keys in %replace hash
+    $line =~ s/($replace_regex)/$replace{$1}/g;
+
+    # Replace file names
+    $line =~ s/($file_regex)/$file_replacements{$1}/g;
+
+    # Replace keys with boundary checks
+    #$line =~ s/($before)($key_regex)($after)/$1$key_replacements{$2}$3/g;
+    while ($line =~ s/($before)($key_regex)($after)/$1$key_replacements{$2}$3/g) {}   
+
+    # Write the modified line to the output file
+    print $output_fh $line;
+}
+  
 
 # Close the input and output files
 close($input_fh);
 close($output_fh);
 }
+
+
 
 
 #save each file with apending noc_id
